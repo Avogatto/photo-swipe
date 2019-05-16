f4ddcbb1e5f9d3d11fd627fbd2092f3c87e7a187
Sara Rubin <sararubin82@gmail.com>
2019-05-16 08:03:32 -0400

    feat: add firebase connection

--- api/user/data-access.js
+++ api/user/data-access.js
@@ -1,5 +1,28 @@
-async function getShareTokens(userId) {
-  return;
+const firebase = require('firebase');
+const db = firebase.firestore();
+
+function addShareToken(userEmail, shareToken) {
+  const userRef = db.collection('users').doc(userEmail);
+  userRef.update({
+    shareTokens: firebase.firestore.FieldValue.arrayUnion(shareToken)
+  }).then(() => {
+    console.log('success!')
+  })
+  .catch(err => {
+    console.error(err);
+    throw err;
+  });
 }
 
-export { getShareTokens };
+function getShareTokens(userEmail) {
+  db.collection('users').doc(userEmail).get()
+  .then((user) => {
+    return user.data().shareTokens;
+  })
+  .catch(err => {
+    console.error(err);
+    throw err;
+  });
+}
+
+module.exports = { getShareTokens, addShareToken };

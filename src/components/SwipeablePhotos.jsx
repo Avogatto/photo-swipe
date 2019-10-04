0d0bdaa9722c46aa871f7d1783f47a483079a99a
Mackenzie Turner <turner.mackenzie.m@gmail.com>
2019-10-04 15:37:01 -0400

    Fix positioning

--- src/components/SwipeablePhotos.jsx
+++ src/components/SwipeablePhotos.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
 import ReactSwing from 'react-swing';
 
 const {
@@ -7,8 +6,6 @@ const {
 } = ReactSwing;
 
 export default class SwipeablePhotos extends React.Component {
-  stackEl = React.createRef();
-
   constructor(props) {
     super(props);
     this.state = { stack: null };
@@ -17,37 +14,32 @@ export default class SwipeablePhotos extends React.Component {
   render() {
     const { handleApproval, photos } = this.props;
     return (
-      <Container textAlign="center">
-        <ReactSwing
-          setStack={stack => this.setState({ stack })}
-          ref={this.stackEl}
-        >
-          {photos.map(({ id, baseUrl, filename }, key) => (
-            <img
-              key={key}
-              ref={`card${key + 1}`}
-              throwout={({ target, throwDirection }) => {
-                if (throwDirection === RIGHT) {
-                  console.log('YOU APPROVED');
-                  handleApproval(id);
-                  target.remove();
-                } else if (throwDirection === LEFT) {
-                  target.remove();
-                }
-              }}
-              src={`${baseUrl}=w${400}-h${400}`}
-              alt={filename}
-              style={{
-                maxHeight: '70vh',
-                maxWidth: '80vw',
-                position: 'absolute',
-                left: '50%',
-                transform: 'translate(-50%)',
-              }}
-            />
-          ))}
-        </ReactSwing>
-      </Container>
+      <ReactSwing setStack={stack => this.setState({ stack })}>
+        {photos.map(({ id, baseUrl, filename }, key) => (
+          <img
+            key={key}
+            throwout={({ target, throwDirection }) => {
+              if (throwDirection === RIGHT) {
+                console.log('YOU APPROVED');
+                handleApproval(id);
+                target.remove();
+              } else if (throwDirection === LEFT) {
+                target.remove();
+              }
+            }}
+            src={`${baseUrl}=w${400}-h${400}`}
+            alt={filename}
+            style={{
+              maxHeight: '70vh',
+              maxWidth: '80vw',
+              position: 'absolute',
+              right: 0,
+              left: 0,
+              margin: 'auto',
+            }}
+          />
+        ))}
+      </ReactSwing>
     );
   }
 }
